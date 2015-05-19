7b6b90337afc92eb3b0a9cc655665a0b5357e5c4
Thesabel Tuto <thesabeltuto@gmail.com>
2015-05-20 03:06:07 +0800

    Version 1.1.0

---
+++ framework/filters.php
@@ -0,0 +1,12 @@
+<?php
+//error_reporting(E_ALL); ini_set('display_errors', 1);		
+//
+//function change_wp_title( $title, $sep ) {
+//	if ( is_archive() ) { 
+//		return 'test';
+//	}
+//}
+//
+//add_filter( 'wp_title', 'change_wp_title', 10, 2 );
+
+?>
