66cb96236400f99d653f8420b7792099da8d242e
Thesabel Tuto <thesabeltuto@gmail.com>
2016-07-02 03:11:51 +0800

    Version 1.1.2

--- functions.php
+++ functions.php
@@ -1,11 +1,12 @@
 <?php
 // Global define variables
-define('THEME_FILE', __FILE__ );
-define('THEME_DIR', get_template_directory().'-geo');
-define('THEME_URL', get_template_directory_uri().'-geo');
+$THEME_CHILD = '-geo';
+define('THEME_CHILD_FILE', __FILE__ );
+define('THEME_CHILD_DIR', get_template_directory().$THEME_CHILD);
+define('THEME_CHILD_URL', get_template_directory_uri().$THEME_CHILD);
 
 // Global variables
-$THEME_VERSION = '1.1.1';
+$THEME_VERSION = '1.1.2';
 $THEME_CSS_VERSION = '1.0.9';
 
 require_once(THEME_DIR.'/framework/admin.php');
